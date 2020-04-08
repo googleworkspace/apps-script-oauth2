15aefb96034ce1e5d40a4f55b8280d10ec96d69e
Eric Koleda <ekoleda@google.com>
2020-04-07 20:02:15 -0400

    Resolve (most) audit issues.

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,18 @@ const del = require('del');
 const rename = require("gulp-rename");
 const eslint = require('gulp-eslint');
 
-gulp.task('dist', ['clean'], function() {
-  gulp.src('src/*.js')
-      .pipe(concat('OAuth2.gs'))
-      .pipe(expose('this', 'OAuth2'))
-      .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function() {
-  del([
+gulp.task('clean', async function() {
+  return del([
     'dist/*'
   ]);
 });
+
+gulp.task('dist', gulp.series('clean', async function(){
+  return gulp.src('src/*.js')
+      .pipe(concat('OAuth2.gs'))
+      .pipe(expose('this', 'OAuth2'))
+      .pipe(gulp.dest('dist'));
+}));
 
 gulp.task('lint', () => {
   return gulp.src(['src/*.js', 'samples/*.gs', 'test/**/*.js', '!node_modules/**'])
